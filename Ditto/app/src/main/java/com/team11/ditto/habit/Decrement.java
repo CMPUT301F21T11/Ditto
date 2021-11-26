92c4d9f3ad557fa0009266adaf836d3dc67f98e2
Kelly Shih <kshih@ualberta.ca>
2021-11-25 18:18:18 -0700

    added some alarm things

--- Ditto/app/src/main/java/com/team11/ditto/habit/Decrement.java
+++ Ditto/app/src/main/java/com/team11/ditto/habit/Decrement.java
@@ -1,4 +1,15 @@
 package com.team11.ditto.habit;
 
-public class Decrement {
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class Decrement extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        //get habits due today
+        //if habitDoneToday is true -> increment streak
+        //if habitDone is false -> decrement streak
+
+    }
 }
