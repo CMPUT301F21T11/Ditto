13b2c9474636efa8851038e452c90c114a41a434
Courtenay Laing-Kobe <laingkob@ualberta.ca>
2021-11-29 01:57:55 -0700

    sorting still being a pain

--- Ditto/app/src/main/java/com/team11/ditto/habit_event/HabitEvent.java
+++ Ditto/app/src/main/java/com/team11/ditto/habit_event/HabitEvent.java
@@ -74,6 +74,7 @@ public class HabitEvent implements Serializable, Comparable {
         setDate();
     }
 
+
     public String getName(){
         return name;
     }
--- Ditto/app/src/test/java/com/team11/ditto/HabitEventUnitTests.java
+++ Ditto/app/src/test/java/com/team11/ditto/HabitEventUnitTests.java
@@ -4,9 +4,12 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import android.util.Log;
+
 import com.team11.ditto.habit_event.HabitEvent;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 
@@ -22,9 +25,34 @@ public class HabitEventUnitTests {
     public void listSort(){
         ArrayList<HabitEvent> events = new ArrayList<>();
         ArrayList<Double> loc = new ArrayList<Double>();
-        
-        events.add(
-          new HabitEvent("habitID", "comment", "photo", (List<Double>) loc, "uid", "name", new Date())
-        );
+        Date date = new Date();
+        HabitEvent first = new HabitEvent("zeventID","zhabitID", "zcomment",
+                "zphoto", (List<Double>) loc, "ztitle", "zuid", "zname", date);
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Date date2 = new Date();
+        HabitEvent second = new HabitEvent("eventID2","habitID2", "comment2",
+                "photo2", (List<Double>) loc, "title2", "uid2", "name2", date2);
+
+        events.add(second);
+        events.add(first);
+
+        events.sort(new Comparator<HabitEvent>() {
+            @Override
+            public int compare(HabitEvent habitEvent, HabitEvent t1) {
+                return habitEvent.getDate().compareTo(t1.getDate());
+            }
+        });
+
+        ArrayList<HabitEvent> events2 = new ArrayList<>();
+        events2.add(first);
+        events2.add(second);
+
+       assertTrue(events.equals(events2));
     }
 }
